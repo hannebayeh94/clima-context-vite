fc183b84fdd2a4809c95176a86e2a66f52df52d0
Hanne Bayeh <hannejosebayehvillarreal@gmail.com>
2022-08-12 11:21:06 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import useClima from "../hooks/useClima";
+
+const Formulario = () => {
+  const [alerta, setAlerta] = useState(false);
+  const { busqueda, datosBusqueda, consultarClima } = useClima();
+  const { ciudad, pais } = busqueda;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Ingrese una ciudad y un país");
+      return;
+    }
+    setAlerta('');
+    consultarClima(busqueda);
+  };
+
+  return (
+    <div className="contenedor">
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input
+            type="text"
+            id="ciudad"
+            name="ciudad"
+            onChange={datosBusqueda}
+            value={ciudad}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="pais">Pais</label>
+          <select name="pais" id="pais" onChange={datosBusqueda} value={pais}>
+            <option value="">--seleccione un pais--</option>
+            <option value="US">Estados unidos</option>
+            <option value="MX">Mexico</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">Espain</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Consultar clima" />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
